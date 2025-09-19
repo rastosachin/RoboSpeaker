d3ffe717ef1a61f942c83ce814debecad0cefa1e
rastosachin <rastosachin.second@gmail.com>
2025-09-19 19:25:49 +0545

    Fully functional Robospeaker v1.1

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import pyttsx3
 
-# if __name__ == '__main__':
-#     print("Welcome to RoboSpeaker")
-#
-#     engine = pyttsx3.init()
-#
-#     while True:
-#         x = input("Enter what you want me to speak or 'q' to quit: ")
-#         if x.lower() == "q":
-#             engine.say("Goodbye! Have a nice day!")
-#             engine.runAndWait()
-#             break
-#         engine.say(x)
-#         engine.runAndWait()
-#         engine.stop()
+def speak(audio):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
+    voices = engine.getProperty('voices')
+    # engine.setProperty('voice', voices[0].id)
+    engine.setProperty('voice', voices[1].id)
+    engine.say(audio)
+    engine.runAndWait()
+    engine.stop()
 
-engine = pyttsx3.init()
-engine.say("Hello World")
-engine.runAndWait()
-engine.stop()
-
-engine.say("This is Robo Speaking")
-engine.runAndWait()
+while True:
+    audio = input("Enter something to be said: ").strip()
+    if not audio:
+        continue
+    if audio.lower() in ("quit", "exit"):
+        speak("Goodbye")
+        break
+    speak(audio)
