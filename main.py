854ee149f7f4f4ab91e252ef4e9fe153d5e6f86d
rastosachin <rastosachin.second@gmail.com>
2025-09-20 07:17:27 +0545

    Updated RoboSpeaker v1.2

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ def speak(audio):
     engine = pyttsx3.init()
     engine.setProperty('rate', 150)
     voices = engine.getProperty('voices')
-    # engine.setProperty('voice', voices[0].id)
-    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('voice', voices[0].id)
+    # engine.setProperty('voice', voices[1].id)
     engine.say(audio)
     engine.runAndWait()
     engine.stop()
 
+username = input("Enter your username to continue to RoboSpeaker: ")
+print(f'Hello {username}! I am your Voice Assistant Jarvis v1.1.\nDesigned for Windows. Developed by Sachin Rastogi.\n')
+
+speak(f'Hello {username}, this is Jarvis, your personal Voice Assistant v1.1.')
 while True:
-    audio = input("Enter something to be said: ").strip()
+    audio = input("Please enter what you want me to speak: ").strip()
     if not audio:
         continue
     if audio.lower() in ("quit", "exit"):
-        speak("Goodbye")
+        speak("Exiting Jarvis.")
         break
     speak(audio)
